06323dab05cf592d88c40f4aa859ef6792a3401d
CristianVinna <moura.cristian@escola.pr.gov.br>
2026-03-25 08:25:02 -0300

    Create desafio1.py
     Exibe uma mensagem pedindo o primeiro número

---
+++ desafio1.py
@@ -0,0 +1,19 @@
+# Programa que soma dois números
+
+# Exibe uma mensagem pedindo o primeiro número
+print("Digite o primeiro número:")
+
+# Recebe o primeiro número digitado pelo usuário e converte para número decimal
+numero1 = float(input())
+
+# Exibe uma mensagem pedindo o segundo número
+print("Digite o segundo número:")
+
+# Recebe o segundo número digitado pelo usuário e converte para número decimal
+numero2 = float(input())
+
+# Soma os dois números digitados
+soma = numero1 + numero2
+
+# Mostra o resultado da soma na tela
+print("A soma dos números é:", soma)
