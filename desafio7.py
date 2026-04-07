e57ae83067f592e1043383f39d24fd056235df47
CristianVinna <moura.cristian@escola.pr.gov.br>
2026-04-07 09:18:01 -0300

    Desafio7.py

--- desafio7.py
+++ desafio7.py
@@ -1,10 +1,10 @@
-# Solicita ao usuário o capital inicial (valor investido)
+# Pede para o usuário o capital inicial (valor investido)
 capital = float(input("Digite o capital (R$): "))
 
-# Solicita ao usuário a taxa de juros (em porcentagem)
+# Pede ao usuário a taxa de juros (em porcentagem)
 taxa = float(input("Digite a taxa de juros (%): "))
 
-# Solicita ao usuário o tempo (em períodos, ex: meses ou anos)
+# Pede ao usuário o tempo (em períodos, ex: meses ou anos)
 tempo = float(input("Digite o tempo: "))
 
 # Converte a taxa de porcentagem para decimal dividindo por 100
@@ -13,5 +13,5 @@ taxa_decimal = taxa / 100
 # Calcula os juros simples usando a fórmula J = C * i * t
 juros = capital * taxa_decimal * tempo
 
-# Exibe o valor dos juros calculados
+# Mostra o valor dos juros calculados
 print("O valor dos juros é: R$", juros)
